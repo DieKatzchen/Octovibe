121518203d829c00558eaf683741976c62570f79
DieKatzchen <himitsu73@hotmail.com>
2025-10-21 14:57:10 -0400

    Fixed the indents

--- firmware/Octovibe/Octovibe.ino
+++ firmware/Octovibe/Octovibe.ino
@@ -22,13 +22,13 @@ void tcode_D2() {
 
 void tcode_DSTOP() {
     digitalWrite(V0_PIN, LOW);
-		digitalWrite(V1_PIN, LOW);
-		digitalWrite(V2_PIN, LOW);
-		digitalWrite(V3_PIN, LOW);
-		digitalWrite(V4_PIN, LOW);
-		digitalWrite(V5_PIN, LOW);
-		digitalWrite(V6_PIN, LOW);
-		digitalWrite(V7_PIN, LOW);
+	digitalWrite(V1_PIN, LOW);
+	digitalWrite(V2_PIN, LOW);
+	digitalWrite(V3_PIN, LOW);
+	digitalWrite(V4_PIN, LOW);
+	digitalWrite(V5_PIN, LOW);
+	digitalWrite(V6_PIN, LOW);
+	digitalWrite(V7_PIN, LOW);
 }
 
 //void tcode_DUPDATE() {
@@ -61,16 +61,16 @@ TCode tcode(reinterpret_cast<TCodeAxis *>(&axes), sizeof(axes) / sizeof(TCodeAxi
 
 void setup()
 {
-	  //validate_ota();
-		pinMode(DEBUG_LED, OUTPUT);
-		pinMode(V0_PIN, OUTPUT);
-		pinMode(V1_PIN, OUTPUT);
-		pinMode(V2_PIN, OUTPUT);
-		pinMode(V3_PIN, OUTPUT);
-		pinMode(V4_PIN, OUTPUT);
-		pinMode(V5_PIN, OUTPUT);
-		pinMode(V6_PIN, OUTPUT);
-		pinMode(V7_PIN, OUTPUT);
+	//validate_ota();
+	pinMode(DEBUG_LED, OUTPUT);
+	pinMode(V0_PIN, OUTPUT);
+	pinMode(V1_PIN, OUTPUT);
+	pinMode(V2_PIN, OUTPUT);
+	pinMode(V3_PIN, OUTPUT);
+	pinMode(V4_PIN, OUTPUT);
+	pinMode(V5_PIN, OUTPUT);
+	pinMode(V6_PIN, OUTPUT);
+	pinMode(V7_PIN, OUTPUT);
     digitalWrite(DEBUG_LED, HIGH);
     comm = new BLECommunication();
 		comm->start();
@@ -79,13 +79,13 @@ void setup()
 void loop()
 {
     if (comm->isOpen()){
-      digitalWrite(DEBUG_LED, HIGH);
+		digitalWrite(DEBUG_LED, HIGH);
 			char buffer[100];
 	    if(comm->readData(buffer)){
   			uint32_t t0 = micros();
 	  		tcode.parse_single_line(buffer, strlen(buffer));
 				#ifdef BT_ECHO
-        Serial.println(axes.V0.get_remap(t0));
+			Serial.println(axes.V0.get_remap(t0));
 				Serial.println(axes.V1.get_remap(t0));
 				Serial.println(axes.V2.get_remap(t0));
 				Serial.println(axes.V3.get_remap(t0));
@@ -95,8 +95,8 @@ void loop()
 				Serial.println(axes.V7.get_remap(t0));
 				#endif
 		  	analogWrite(V0_PIN, axes.V0.get_remap(t0));
-			 	analogWrite(V1_PIN, axes.V1.get_remap(t0));
-			  analogWrite(V2_PIN, axes.V2.get_remap(t0));
+			analogWrite(V1_PIN, axes.V1.get_remap(t0));
+			analogWrite(V2_PIN, axes.V2.get_remap(t0));
   			analogWrite(V3_PIN, axes.V3.get_remap(t0));
   			analogWrite(V4_PIN, axes.V4.get_remap(t0));
   			analogWrite(V5_PIN, axes.V5.get_remap(t0));
@@ -104,10 +104,10 @@ void loop()
 		  	analogWrite(V7_PIN, axes.V7.get_remap(t0));
 			}
 		}
-   else{
-    digitalWrite(DEBUG_LED,HIGH);
-    delay(1000);
-    digitalWrite(DEBUG_LED,LOW);
-    delay(1000);
+	else{
+		digitalWrite(DEBUG_LED,HIGH);
+		delay(1000);
+		digitalWrite(DEBUG_LED,LOW);
+		delay(1000);
    }
 }
